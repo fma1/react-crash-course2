875387beb7fc4c1d96bcd384a8350f2155f43c7a
Fred Ma <fredma1234@gmail.com>
2023-11-12 21:25:17 -0500

    Finished 03 - JSX

---
+++ src/03 - JSX/App.js
@@ -0,0 +1,44 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+/*
+  const name = 'Conner';
+  return (
+    <>
+      <h1>Hello {name.toUpperCase()}</h1>
+      <p>Test</p>
+    <>
+  );
+*/
+/*
+  const error = true;
+  return (
+    <>
+      {error && <h1>Error</h1>}
+      {!error ? <h1>Success</h1> : null}
+    </>
+  );
+*/
+/*
+  const props = {
+    id: 'input',
+    type: 'text',
+    maxLength: 3
+  };
+
+  return (
+    <>
+      <label htmlFor="input">Input: </label>
+      <input {...props} placeholder="user" />
+    </>
+  );
+*/
+  return <p style={{
+    color: 'red',
+    textAlign: 'center',
+    fontSize: '48px'
+  }}>Hello World</p>
+}
+
+export default App;
