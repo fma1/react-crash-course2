7e0ecda0f08b5d0e9c4cfffa11a6df1153def166
Fred Ma <fredma1234@gmail.com>
2023-11-27 17:31:23 -0500

    Finished 07 - Component Lifecycle

--- src/07 - Component Lifecycle/Counter.js
+++ src/07 - Component Lifecycle/Counter.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useLayoutEffect, useState } from 'react';
 import logo from '../logo.svg';
 import '../App.css';
 
@@ -11,8 +11,59 @@ function Counter() {
   const [ bool, setBool ] = useState(false);
 
   useEffect(() => {
+    console.log('mounted');
+
+    return () => console.log('unmount');
+  }, []);
+
+  useEffect(() => {
+    console.log('count changed');
+
+    return () => console.log('cleanup count changed');
+  }, [count]);
+
+// Infinite loop
+/*
+  useEffect(() => {
+    // So doesn't crash browser
+    if (count > 20) return;
+    setCount(count + 1);
+  }, [count]);
+*/
+
+  useEffect(() => {
+    console.log('pressed re-render');
+  }, [bool]);
+
+  useEffect(() => {
     console.log('render');
   });
+
+  useEffect(() => {
+    console.log('mounted');
+  }, []);
+
+  // Causes flash of 3
+  /*
+  useEffect(() => {
+    if (count === 3) {
+      setCount(4);
+    }
+  }, [count]);
+  */
+
+  // runs synchronously after render
+  // not painted to screen until useLayoutEffect has finished running
+  useLayoutEffect(() => {
+    if (count === 3) {
+      setCount(4);
+    }
+  }, [count]);
+
+
+  // Create extra delay to see flash of count of 3
+  const startTime = new Date();
+  while (new Date() - startTime < 100) {}
 
   return (
     <>
