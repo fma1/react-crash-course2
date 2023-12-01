56d2496b5281c24168bd7c025f7f4f44d6f32336
Fred Ma <fredma1234@gmail.com>
2023-12-01 16:57:21 -0500

    Finished 08 - Refs

--- src/08 - Refs/App.js
+++ src/08 - Refs/App.js
@@ -1,18 +1,43 @@
-import { useReducer, useState } from 'react';
+import { forwardRef, useRef, useState } from 'react';
 import logo from '../logo.svg';
 import '../App.css';
 
+const MyInput = forwardRef(function (props, ref) {
+  return <input ref={ref} {...props} style={{color: 'red'}} />
+});
+
+// callback ref
+// ref={} will only run on initial mount
+// Also runs on unmount but passes in null instead of domNode
+function handleRef(domNode) {
+  console.log(domNode);
+}
+
 function App() {
-  const [seconds, setSeconds] == useState(0);
+/*
+  const [seconds, setSeconds] = useState(0);
+  // const [timerID, setTimerID] = useState(null);
+  // Doesn't work because setInterval() causes re-render making it undefined
+  // on all consecutive re-renders
+  // let timerID;
+  // Persists across renders but changing the value does not cause a re-render
+  const timerID = useRef(null);
 
   const startTimer = () => {
-    setInterval(() => {
+// setTimerID() is suboptimal because causing an unnecessary re-render
+// We could possible use a useState({current: null}) and keep using the same object
+// to avoid a re-render
+//     setTimerID(setInterval(() => {
+//       setSeconds(currSeconds => currSeconds + 1);
+//     }, 1000));
+
+    timerID.current = setInterval(() => {
       setSeconds(currSeconds => currSeconds + 1);
     }, 1000);
   };
 
   const stopTimer = () => {
-
+    clearInterval(timerID.current);
   };
 
   return (
@@ -20,6 +45,26 @@ function App() {
       <button onClick={startTimer}>Start</button>
       <button onClick={stopTimer}>Stop</button>
       <p>seconds: {seconds}</p>
+    </>
+  );
+*/
+  const inputRef = useRef(null);
+  // inputRef.current does not get set until after initial render
+  // So can't be used on initial render
+  console.log("inputRef.current: " + inputRef.current);
+
+  const focusInput = () => {
+    inputRef.current.focus();
+  };
+
+  return (
+    <>
+      {/* React sets inputRef.current to be the DOM node associated with this JSX element */}
+      {/* ref attributes by default can only be added to DOM nodes and not custom components */}
+      <MyInput ref={inputRef} />
+      <button onClick={focusInput}>Focus</button>
+      <br />
+      <MyInput ref={handleRef} />
     </>
   );
 }
---
+++ src/App.js
@@ -0,0 +1,1 @@
+08 - Refs/App.js
